7553d16f05fb6e634a0f3883495154f9c1db145d
Amit Shree Chandan <amitshreechandan@gmail.com>
2021-09-25 12:55:50 +0530

    changes to new.cpp

--- new.cpp
+++ new.cpp
@@ -9,9 +9,21 @@ void fun(int n)
         cout << n << ' ';
     }
 }
+
+void fun2(int n)
+{
+    if (n > 0)
+    {
+        cout << n << ' ';
+        fun2(n - 1);
+    }
+}
+
 int main()
 {
     int x = 3;
     fun(3);
+    fun2(3);
+    
     return 0;
 }
